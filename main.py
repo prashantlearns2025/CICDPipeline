5cdca7b322e7e6d76b01a43b3f985175b0954199
Prashant <prashantsharma2921996@gmail.com>
2025-01-29 16:56:13 +0530

    commit 1st

---
+++ main.py
@@ -0,0 +1,50 @@
+from fastapi import FastAPI
+from openai import OpenAI
+import config
+import uvicorn
+import os
+from pydantic import BaseModel
+
+
+assistant_id = config.assistant_id
+api_key = os.environ['OPENAI_API']    # config.api_key
+client = OpenAI(api_key=api_key)
+
+
+app = FastAPI()
+
+class Body(BaseModel):
+    text: str
+
+@app.get("/")
+def welcome():
+    return {"message": "Welcome to ChatGPT AI App"}
+
+@app.post("/response")
+def generate(body: Body):
+
+    prompt =  body.text
+    thread = client.beta.threads.create()
+    messages = client.beta.threads.messages.create(
+        thread_id = thread.id,  
+        role = "user",
+        content = prompt
+    )
+
+    run = client.beta.threads.runs.create(
+    thread_id=thread.id,
+    assistant_id=assistant_id
+    )
+
+    while True:
+        run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
+        if run.status == "completed":
+            messages = client.beta.threads.messages.list(thread_id=thread.id)
+            latest_message = messages.data[0].content[0].text.value
+            break;
+
+    return latest_message
+
+
+if __name__ == "__main__":
+    uvicorn.run(app,host = "0.0.0.0",port=80)
